873638282863342a743b65e500c78af4a9a1f8ae
Dhruv <190310patel@gmail.com>
2022-03-07 19:13:19 +0530

    HomePage Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { Route } from "react-router-dom"
+import { Switch } from "react-router-dom/cjs/react-router-dom.min"
 import { ThemeProvider } from "styled-components"
-import { darkTheme } from "./components/Themes"
+import  lightTheme  from "./components/Themes"
 import GlobalStyle from "./globalStyle"
+import  Main  from "./components/Main"
+import  AboutPage  from "./components/AboutPage"
+import  BlogPage from "./components/BlogPage"
+import  WorkPage  from "./components/WorkPage"
+import  MySkillsPage  from "./components/MySkillsPage"
+
+
+
 function App() {
   return <>
 
 
 
     <GlobalStyle/>
-    <ThemeProvider theme={darkTheme}>
-      App File
+    <ThemeProvider theme={lightTheme}>
+      <Switch>
+        <Route exact path="/" component={Main}/>
+        <Route exact path="/about" component={AboutPage}/>
+        <Route exact path="/blog" component={BlogPage}/>
+        <Route exact path="/work" component={WorkPage}/>
+        <Route exact path="/skills" component={MySkillsPage}/>
+      </Switch>
     </ThemeProvider>
     
     
--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,4 +1,4 @@
-export const lightTheme = {
+const lightTheme = {
     
     body:"#FCF6F4",
     text:"#000000",
@@ -7,12 +7,13 @@ export const lightTheme = {
     textRgba:"0,0,0",
 
 }
+export default lightTheme
 
-export const darkTheme = {
+// export const darkTheme = {
 
-    body:"#000000",
-    text:"#FCF6F4",
-    fontFamily:"'Source Sans Pro', sans-serif",
-    textRgba : "252, 246, 244",
-    bodyRgba:"0,0,0",
-}
+//     body:"#000000",
+//     text:"#FCF6F4",
+//     fontFamily:"'Source Sans Pro', sans-serif",
+//     textRgba : "252, 246, 244",
+//     bodyRgba:"0,0,0",
+// }
