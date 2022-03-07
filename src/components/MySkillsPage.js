e82af60031f4ad23961a381316debd7de996b32b
Dhruv <190310patel@gmail.com>
2022-03-07 13:39:50 +0530

    Add components

---
+++ src/components/MySkillsPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MySkillsPage = () => {
+  return (
+    <div>MySkillsPage Component/page</div>
+  )
+}
+
+export default MySkillsPage
---
+++ src/components/WorkPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WorkPage = () => {
+  return (
+    <div>WorkPage Component/page</div>
+  )
+}
+
+export default WorkPage
